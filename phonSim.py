f5ac9a92be2cd3e0bc218e0646704e07e3ffcb93
pgeorgis <philip.georgis@gmail.com>
2023-08-07 18:29:16 +0300

    Optimized strip_diacritics() and normalize_ipa_ch() functions

--- phonSim.py
+++ phonSim.py
@@ -34,8 +34,11 @@ def strip_diacritics(string, excepted=[]):
     """Removes diacritic characters from an IPA string
     By default removes all diacritics; in order to keep certain diacritics,
     these should be passed as a list to the "excepted" parameter"""
-    try:
+    if len(excepted) > 0:
         to_remove = ''.join([d for d in diacritics if d not in excepted])
+    else:
+        to_remove = ''.join(diacritics)
+    try:
         return re.sub(f'[{to_remove}]', '', string)
 
     except RecursionError:
@@ -48,8 +51,11 @@ def strip_diacritics(string, excepted=[]):
 def normalize_ipa_ch(string, ipa_norm_map=ipa_norm_map):
     """Normalizes some commonly mistyped IPA characters according to a pre-loaded normalization mapping dictionary"""
 
-    for ch, repl in ipa_norm_map.items():
-        string = re.sub(re.escape(ch), repl, string)
+    def replace_callback(match):
+        return ipa_norm_map[match.group(0)]
+
+    pattern = re.compile('|'.join(map(re.escape, ipa_norm_map.keys())))
+    string = pattern.sub(replace_callback, string)
 
     return string
 
