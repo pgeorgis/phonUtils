ec84ee1d0d0c58f88b971cdd40212ed57339701a
pgeorgis <philip.georgis@gmail.com>
2025-08-23 13:50:31 +0200

    Add autonomous diacritic segmentation tests

--- tests/test_phonSim.py
+++ tests/test_phonSim.py
@@ -15,6 +15,43 @@ def test_segmentation():
         assert segment_ipa(ipa) == segments
 
 
+def test_autonomous_diacritic_segmentation():
+    autonomous_diacritics = {'ˈ', 'ˀ', 'ˤ', '̌', '̀', '̂', '́'}
+    
+    string1 = 'dʲæ̌ːɕɪmt'
+    ref1a = ['dʲ', 'æː', '̌', 'ɕ', 'ɪ', 'm', 't']
+    ref1b = ['dʲ', 'æ̌ː', 'ɕ', 'ɪ', 'm', 't']
+    
+    string2 = 'dɔ́ːwɡ̊'
+    ref2a = ['d', 'ɔː', '́', 'w', 'ɡ̊']
+    ref2b = ['d', 'ɔ́ː', 'w', 'ɡ̊']
+
+    string3 = 'ʥˈɛɕɛɲʨ'
+    ref3a = ['ʥ', 'ɛ', 'ˈ', 'ɕ', 'ɛ', 'ɲ', 'ʨ']
+    ref3b = ['ʥ', 'ˈɛ', 'ɕ', 'ɛ', 'ɲ', 'ʨ']
+
+    string4 = 'ɡalˀwáːˀ'
+    ref4a = ['ɡ', 'a', 'l', 'ˀ', 'w', 'aː', '́ˀ']
+    ref4b = ['ɡ', 'a', 'lˀ', 'w', 'áːˀ']
+
+    string5 = 'ʧˤɑːv'
+    ref5a = ['ʧ', 'ˤ', 'ɑː', 'v']
+    ref5b = ['ʧˤ', 'ɑː', 'v']
+    
+    # Test with autonomous diacritic segmentation
+    for string_i, ref_i in zip(
+        [string1, string2, string3, string4, string5],
+        [ref1a, ref2a, ref3a, ref4a, ref5a],
+    ):
+        assert segment_ipa(string_i, autonomous_diacritics=autonomous_diacritics) == ref_i
+    # Test without autonomous diacritic segmentation
+    for string_i, ref_i in zip(
+        [string1, string2, string3, string4, string5],
+        [ref1b, ref2b, ref3b, ref4b, ref5b],
+    ):
+        assert segment_ipa(string_i, autonomous_diacritics=None) == ref_i
+ 
+
 def test_phone_sim_symmetrical():
     """Test that phone_sim(x, y) == phone_sim(y, x)."""
     for measure in [
