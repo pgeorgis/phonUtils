452e314b031e161221bd4975c0972c7381bdc384
pgeorgis <philip.georgis@gmail.com>
2026-01-21 00:17:12 +0100

    Add progressive voicing assimilation function

--- phonUtils/phonTransforms.py
+++ phonUtils/phonTransforms.py
@@ -76,6 +76,54 @@ def regressiveVoicingAssimilation(ipa_string: str,
     return ipa_string
 
 
+def progressiveVoicingAssimilation(ipa_string: str,
+                                   devoice_dict: dict = DEFAULT_DEVOICE_DICT,
+                                   voicing_dict: dict = None,
+                                   to_voiceless: bool = True,
+                                   to_voiced: bool = True,
+                                   exception: Iterable = [],
+                                   ):
+    original = ipa_string[:]
+    
+    if voicing_dict is None:
+        # Reverse mapping for voicing
+        voicing_dict = {v: k for k, v in devoice_dict.items()}
+
+    voiced_str = '|'.join(devoice_dict.keys())
+    voiceless_str = '|'.join(devoice_dict.values())
+
+    # C1 voiced, C2 voiceless -> C2 becomes voiced
+    if to_voiced:
+        for voiceless, voiced in voicing_dict.items():
+            # Look for voiceless C2 preceded by voiced C1
+            ipa_string = re.sub(
+                rf'(?<=[{voiced_str}{VOICED_CONSONANTS}]){voiceless}(?![̬])',
+                voiced,
+                ipa_string
+            )
+
+    # C1 voiceless, C2 voiced -> C2 becomes voiceless
+    if to_voiceless:
+        for voiced, voiceless in devoice_dict.items():
+            # Look for voiced C2 preceded by voiceless C1
+            ipa_string = re.sub(
+                rf'(?<=[{voiceless_str}{VOICELESS_CONSONANTS}]){voiced}(?![̥̊])',
+                voiceless,
+                ipa_string
+            )
+
+    # Postprocess any misplaced diacritics
+    ipa_string = re.sub(r'ʲ([̥̊])', r'\1ʲ', ipa_string)
+
+    # Cancel the assimilation if it results in an illegal sequence
+    for exc in exception:
+        if re.search(exc, ipa_string):
+            if not re.search(exc, original):
+                return original
+
+    return ipa_string
+
+
 def degeminate(ipa_string: str,
                phones: Iterable = CONSONANTS,
                ):
